451ebfe83df93edf3056ef82849bf10765e70361
hmg <tmdnldjazz@gmail.com>
2015-02-15 22:46:31 +0900

    none

--- MapTool/MainGame.cs
+++ MapTool/MainGame.cs
@@ -12,10 +12,14 @@ using System.Windows.Forms;
 namespace MapTool
 {    
     public partial class MainGame:IDisposable
-    {   
-        
+    {
+        private bool[] keys = new bool[256];
+
         public bool Setup(MainForm mainForm)
         {
+            mainForm.KeyDown += new KeyEventHandler(this.mainForm_KeyDown);            
+            mainForm.KeyUp += new KeyEventHandler(this.mainForm_KeyUp);
+
             try
             {
                 this.CreateDevice(mainForm);
@@ -34,6 +38,22 @@ namespace MapTool
             return true;
         }
 
+        private void mainForm_KeyUp(object sender, KeyEventArgs e)
+        {
+            if ((int)e.KeyCode < this.keys.Length)
+            {
+                this.keys[(int)e.KeyCode] = false;
+            }
+        }
+
+        void mainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((int)e.KeyCode < this.keys.Length)
+            {
+                this.keys[(int)e.KeyCode] = true;
+            }
+        }
+
         /// <summary>
         /// 메인 루프 처리
         /// </summary>
@@ -42,7 +62,17 @@ namespace MapTool
             this.m_device.Clear(ClearFlags.Target | ClearFlags.ZBuffer, 
                 System.Drawing.Color.DarkBlue, 1.0f, 0);
             this.m_device.BeginScene();
-
+            this.m_font.DrawText(null, "키보드 입력받기", 0, 0, System.Drawing.Color.White);
+            int height = 24;
+            for (int i = 0; i < this.keys.Length; i++)
+            {
+                if (this.keys[i])
+                {
+                    this.m_font.DrawText(null, ((Keys)i).ToString(), 0, height,
+                    System.Drawing.Color.White);
+                    height += 24;
+                }
+            }
             this.m_device.EndScene();
             this.m_device.Present();
         }
